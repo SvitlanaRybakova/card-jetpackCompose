7cefe65a13d0bf71bc88e3e3a5af86e35f1f2c3f
Svitlana <rsv0011@gmail.com>
2024-03-12 20:57:36 +0100

    feat: implenment the basic card

--- app/src/main/java/com/example/card/MainActivity.kt
+++ app/src/main/java/com/example/card/MainActivity.kt
@@ -3,44 +3,75 @@ package com.example.card
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.card.ui.theme.CardTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            CardTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+            Column() {
+                ListItem("Ulrika Andersson", "The artist")
+                ListItem("Ulrika Andersson", "The artist")
+                ListItem("Ulrika Andersson", "The artist")
+                ListItem("Ulrika Andersson", "The artist")
+                ListItem("Ulrika Andersson", "The artist")
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
+//@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    CardTheme {
-        Greeting("Android")
+fun ListItem(name: String, prof: String) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(10.dp),
+        shape = RoundedCornerShape(15.dp),
+        elevation = CardDefaults.elevatedCardElevation(5.dp)
+    ) {
+        Box() {
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Image(
+                    painter = painterResource(id = R.drawable.avatar),
+                    contentDescription = name,
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier
+                        .padding(5.dp)
+                        .size(64.dp)
+                        .clip(CircleShape)
+                )
+                Column(modifier = Modifier.padding(start = 16.dp)) {
+                    Text(text = name)
+                    Text(text = prof)
+                }
+
+            }
+
+        }
     }
 }
