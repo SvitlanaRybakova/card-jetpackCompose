f2fbf242d365421751c556c6f2b6a07d40e49aac
Svitlana <rsv0011@gmail.com>
2024-03-13 11:17:22 +0100

    feat: add state

--- app/src/main/java/com/example/card/MainActivity.kt
+++ app/src/main/java/com/example/card/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -19,13 +21,18 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.card.ui.theme.CardTheme
 
 class MainActivity : ComponentActivity() {
@@ -34,10 +41,12 @@ class MainActivity : ComponentActivity() {
         setContent {
             Column() {
                 ListItem("Ulrika Andersson", "The artist")
-                ListItem("Ulrika Andersson", "The artist")
-                ListItem("Ulrika Andersson", "The artist")
-                ListItem("Ulrika Andersson", "The artist")
-                ListItem("Ulrika Andersson", "The artist")
+               Box(
+                 modifier = Modifier.fillMaxWidth(),
+                   contentAlignment = Alignment.Center
+               ){
+                   CircleItem()
+               }
             }
         }
     }
@@ -75,3 +84,30 @@ fun ListItem(name: String, prof: String) {
         }
     }
 }
+
+@Composable
+fun CircleItem(){
+    var counter= remember{
+        mutableStateOf(0)
+    }
+    var color= remember{
+        mutableStateOf(Color.Blue)
+    }
+    Box(modifier = Modifier
+        .size(30.dp)
+        .background(color = color.value, shape = CircleShape)
+        .clickable{
+            counter.value ++
+        if(counter.value == 10){
+            color.value = Color.Red
+        }
+        },
+        contentAlignment = Alignment.Center
+    ){
+        Text(
+           style = TextStyle(color = Color.White, fontSize = 20.sp),
+            text = counter.value.toString()
+        )
+    }
+}
+
